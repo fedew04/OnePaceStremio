73d428a456a72b0500854c965b327ae0661dd966
fedew04 <fedewainstein@gmail.com>
2025-06-22 21:25:12 -0300

    wano 52 syrup 3

--- capitulos individuales.bat
+++ capitulos individuales.bat
@@ -1,9 +1,9 @@
 cd C:\Users\fedew\stremio-onepace\OnePaceStremio\stream\series
 @echo off
 
-set arc=EH
-set ep=12
-set hash=748f223628b66fa402bc3b647369098e9e2d6200
+set arc=SY
+set ep=3
+set hash=7e7335f1c1a719c217fd857e3e50b176942ce892
 
 rem wano: WA
 rem egghead: EH
